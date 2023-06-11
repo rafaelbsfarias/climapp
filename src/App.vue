ccc8e71cb0f4ac7b412e7c029b234357d0f8625b
Monique Silva <moniquesntsilva@gmail.com>
2023-06-11 20:38:52 -0300

    add projeto

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <Clima />
+</template>
+
+<script>
+import Clima from "./components/Clima";
+
+export default {
+  name: "App",
+  components: {
+    Clima,
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ src/components/Clima.vue
@@ -0,0 +1,172 @@
+<template>
+  <div class="clima">
+    <div class="clima__componente">
+      <header class="clima__header">
+        <div class="clima__titulo">
+          <h1 class="clima__titulo__texto">Climapp</h1>
+          <p class="clima__titulo__descricao">Previsões climáticas</p>
+        </div>
+        <div class="clima__pesquisa">
+          <input
+            class="clima__pesquisa-input"
+            ref="inputPesquisa"
+            type="text"
+            v-model="localizacao"
+            @keyup.enter="getClima"
+            placeholder="Pesquisar localização"
+          />
+          <button
+            class="clima__pesquisa-botao"
+            @click="getClima"
+          >
+            Pesquisar
+          </button>
+        </div>
+      </header>
+      <main class="clima__conteudo">
+        <!-- Complete a estrutura -->
+      </main>
+    </div>
+  </div>
+</template>
+
+<script>
+const API_KEY = "aa8dc9bdb14443a987921219230306";
+const API_URL = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&lang=pt`;
+
+export default {
+  name: "ClimaApp",
+  data() {
+    return {
+      localizacao: "",
+      climaData: null,
+    };
+  },
+  methods: {
+    getClima() {
+      if (this.localizacao === "") {
+        this.$refs.inputPesquisa.focus();
+        return;
+      }
+
+      // Adicionar a chamada da API weatherapi.com
+      // Armazenar o resultado em climaData
+      console.log(API_KEY, API_URL);
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* Global styles */
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+/* Clima componente styles */
+.clima {
+  min-width: 100vw;
+  min-height: 100vh;
+  font-family: apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  background-color: #f8f9fa;
+  overflow: hidden;
+}
+.clima__componente {
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 2rem;
+  max-width: 1216px;
+  height: 100%;
+  padding: 32px;
+}
+
+@media (max-width: 768px) {
+  .clima__componente {
+    padding: 32px 16px;
+  }
+}
+
+/* Clima header styles */
+.clima__header {
+  margin-bottom: 2rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+}
+
+@media (max-width: 768px) {
+  .clima__header {
+    flex-direction: column;
+    gap: 32px;
+  }
+}
+
+/* Clima titulo styles */
+.clima__titulo__texto {
+  margin: 0;
+  font-weight: 800;
+  font-size: 36px;
+  line-height: 48px;
+  color: #22333b;
+}
+
+.clima__titulo__descricao {
+  margin: 0;
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 32px;
+  color: #495057;
+}
+
+/* Clima pesquisa styles */
+.clima__pesquisa {
+  display: flex;
+  align-items: center;
+  gap: 12px;
+  width: 100%;
+  max-width: 400px;
+}
+
+.clima__pesquisa-input {
+  padding: 12px 17px;
+  width: 100%;
+  border-radius: 6px;
+  border: 1px solid #d1d5db;
+  outline: transparent;
+  transition: all 0.2s ease-in-out;
+  box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.05);
+}
+
+.clima__pesquisa-input:focus {
+  border: 1px solid #f6bd60;
+  outline: 4px solid #f6bd60;
+}
+
+.clima__pesquisa-botao {
+  padding: 12px 17px;
+  border-radius: 6px;
+  border: transparent;
+  outline: transparent;
+  font-weight: 500;
+  color: #000814;
+  background-color: #ffbf69;
+  transition: all 0.2s ease-in-out;
+  box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.05);
+  cursor: pointer;
+}
+
+.clima__pesquisa-botao:hover {
+  background-color: #f6bd60;
+}
+
+.clima__pesquisa-botao:focus {
+  border: 1px solid #f6bd60;
+  outline: 4px solid #f6bd60;
+}
+</style>
